7570ccbd135e3ebef472c6e853bc8dd99dce44a7
samclinton1 <samuelasare001@gmail.com>
2023-06-13 21:50:24 +0300

    changes to exercise.py

--- main/exercise.py
+++ main/exercise.py
@@ -1,26 +1,26 @@
-#http_status = 300
+http_status = 300
 
-#match http_status:
-    #case 200 | 201:
-     #   print('Success')
-    #case 400:
-     #   print('Bad Request')
-    #case 500 | 501:
-     #   print('Server Error')
-    #case _:
-     #   print('Unknown')
+match http_status:
+    case 200 | 201:
+       print('Success')
+    case 400:
+       print('Bad Request')
+    case 500 | 501:
+       print('Server Error')
+    case _:
+       print('Unknown')
 
 
-#favourites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisu', 'Chocolate']
+favourites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisu', 'Chocolate']
 
-#for idx, item in enumerate (favourites):
- #  print(idx, item)
+for idx, item in enumerate (favourites):
+  print(idx, item)
 
-#count = 0
+count = 0
 
-#while count < len(favourites):
- #   print('I like this dessert', favourites[count])
-  #  count += 1
+while count < len(favourites):
+   print('I like this dessert', favourites[count])
+   count += 1
 
 
 
